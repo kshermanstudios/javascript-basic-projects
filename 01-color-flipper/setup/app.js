9ae5e6d63f9a29a7513af0b53c04efd5a98fea19
Kevin <kshermanstudios@gmail.com>
2022-02-14 17:26:20 -0800

    Completed js for button click changing of bkg color and text color value

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,31 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// vars
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    // first task is how to change the bkg when
+    // button is clicked
+    // first check that it works
+    // console.log(document.body);
+
+    // get a random number between 0 and 3
+    // first hard code it like 2 to start dev 
+    const randomNumber = getRandomNumber();
+    console.log('randomNumber ' + randomNumber);
+
+    // the page bg color
+    document.body.style.backgroundColor = colors[randomNumber];
+    // the color name value gets updated as well
+    color.textContent = colors[randomNumber];
+
+});
+
+function getRandomNumber(){
+    // getting unique num between 0 and .99999
+    // then * the array length = 4 = 3.9999
+    // then we need to find the floor of number 
+    // always rounds DOWN... floor gives us 0-3 number
+    return Math.floor( Math.random() * colors.length ); 
+}
