2213e95000f4212d38c276911a6f350da76d691f
Kevin <kshermanstudios@gmail.com>
2022-02-15 17:40:53 -0800

    Completed the show/hide navbar toggle

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -1,5 +1,29 @@
 // classList - shows/gets all classes
+
 // contains - checks classList for specific class
 // add - add class
 // remove - remove class
 // toggle - toggles class
+ 
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+
+navToggle.addEventListener('click', function(e){
+    
+    // console.log(links.classList.contains("show-links"));
+    // console.log(links.classList.contains("links"));
+    
+    // check each classList for specific item
+    /* full way
+
+    if(links.classList.contains('show-links')){
+        links.classList.remove("show-links");
+    } else {
+        links.classList.add("show-links");
+    }
+    */
+
+    links.classList.toggle("show-links");
+   
+});
